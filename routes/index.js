f20e0fc7317c1ae88405aea43777f25b7301b2c2
ET <a12462004@gmail.com>
2017-09-21 02:42:54 +0800

    add member

--- routes/index.js
+++ routes/index.js
@@ -22,9 +22,44 @@ router.post('/search',function(req,res,next){
 				res.send(err);
 			}
 			sql.close();
-			console.log(result.recordset);
+			// console.log(result.recordset);
 			var list = result.recordset;
 			res.send(list);
+		});
+	});
+});
+//會員確認
+router.post('/member',function(req,res,next){
+	var fbId = req.body.id;
+	var fbName = req.body.name;
+	sql.connect(db,function(err){
+		if(err) console.log(err);
+		var request = new sql.Request();
+		//比對是否已經是會員，是的話筆數為1，否的話筆數為0，需要做新增會員的動作。
+		request.query("SELECT count(id) AS count FROM Member WHERE fbId ='"+fbId+"'",function(err,result){
+			if(err) {
+				console.log(err);
+				res.send(err);
+			}
+			var rowsCount = result.recordset[0].count; //筆數
+			// console.log(result.recordset[0].count);
+			if(rowsCount == '0'){ //筆數為0，做會員新增
+				request.query("INSERT INTO Member(fbId,fbName) VALUES('"+fbId+"','"+fbName+"')",function(err,result){
+					if(err){
+						console.log(err);
+						res.send(err);
+					}
+					else{
+						console.log('新增成功.');
+						 sql.close(); //sql要記得關，不然會卡住run不動.
+					}
+				});
+			}
+			else if(rowsCount == '1'){
+				console.log('已是會員.');
+				sql.close(); //sql要記得關，不然會卡住run不動.
+			}
+			
 		});
 	});
 });
