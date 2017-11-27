1b634e992ec305d9e9dbd38565f3f3ab70ea586f
ET <a12462004@gmail.com>
2017-11-27 17:57:50 +0800

    update

---
+++ routes/exam.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var db = require('./lib/db.js');
+var sql = require('mssql');
+/* GET company page. */
+router.get('/', function(req, res, next) {
+	res.render('./exam', {title: '股起勇氣' });
+	
+});
+
+module.exports = router;
