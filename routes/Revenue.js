34c4161393c842ef4858721aef792d2d13536777
ET <a12462004@gmail.com>
2017-12-08 02:10:52 +0800

    modify DividendPolicy and Revenue

--- routes/Revenue.js
+++ routes/Revenue.js
@@ -15,20 +15,20 @@ router.get('/', function(req, res, next) {
         //Replace(value,'.00','')將money型態產生的小數點後面刪掉
            return pool.request().query("WITH [RevenueTable] as "+
 "("+
-  "select code,CONVERT(varchar(7),[r_montly],126) as 'date', CAST([revenue] as INT) 'revenue',[m_growth_rate],[a_growth_rate],"+
-  " CAST([acc_revenue] as INT) 'acc_revenue',"+
+  "select code,CONVERT(varchar(7),[r_montly],111) as 'date', Replace(CONVERT(NVARCHAR(20),CAST(Revenue.revenue AS money),1),'.00','') 'revenue',[m_growth_rate],[a_growth_rate],"+
+  " Replace(CONVERT(NVARCHAR(20),CAST(Revenue.acc_revenue AS money),1),'.00','') 'acc_revenue',"+
   " [acc_growth_rate],"+
   " CAST([last_revenue] as INT) 'last_revenue' from [dbo].[Revenue]"+
   " where code = '"+code+"'"+
 "), [StockPrice] as "+ 
 "("+
-" select top 1  code,[company],CAST ( YEAR(StockPrice.price_date) AS VARCHAR(10) )+'/'+CAST ( MONTH(StockPrice.price_date) AS VARCHAR(10) )+'/'+CAST ( DAY(StockPrice.price_date) AS VARCHAR(10) ) as 'updateDate',[open],[up],[down],[clos],[trading_volume] from "+
+" select top 1  code,[company],CAST ( YEAR(StockPrice.price_date) AS VARCHAR(10) )+'/'+CAST ( MONTH(StockPrice.price_date) AS VARCHAR(10) )+'/'+CAST ( DAY(StockPrice.price_date) AS VARCHAR(10) ) as 'updateDate',[open],[up],[down],[clos],Replace(CONVERT(NVARCHAR(20),CAST(StockPrice.trading_volume AS money),1),'.00','') AS trading_volume from "+
 " [dbo].[StockPrice]"+
 " where code ='"+code+"'"+
 " order by price_date desc "+
 "),[companyprofile] as "+
 "("+
-" select code, [capital],[IndustryType].industry_name from [dbo].[CompanyProfile] join[dbo].[IndustryType]"+
+" select code, Replace(CONVERT(NVARCHAR(20),CEILING(CAST(CompanyProfile.capital AS money)/1000000),1),'.00','') AS capital,[IndustryType].industry_name from [dbo].[CompanyProfile] join[dbo].[IndustryType]"+
 " on  [CompanyProfile].industry = [IndustryType].industry_id where code='"+code+"'"+
 ")"+
 " select [companyprofile].code,date,revenue ,[m_growth_rate],[a_growth_rate], "+
@@ -62,7 +62,7 @@ router.post('/getChartData',function(req,res,next){
 	var code = req.body.code;
 	// console.log(code);
 	new sql.ConnectionPool(db).connect().then(pool => {
-        return pool.request().query("select convert(varchar(7), a.[r_montly], 126) as 'r_montly' , a.[a_growth_rate], cast(a.[revenue] as int) as 'revenue', b.clos from [dbo].[Revenue] a join [dbo].[StockPrice]  b on a.code = b.code and a.r_montly = b.price_date where a.code ='"+code+"' order by a.[r_montly] ")
+        return pool.request().query("select convert(varchar(7), a.[r_montly], 111) as 'r_montly' , a.[a_growth_rate], cast(a.[revenue] as int) as 'revenue', b.clos from [dbo].[Revenue] a join [dbo].[StockPrice]  b on a.code = b.code and a.r_montly = b.price_date where a.code ='"+code+"' order by a.[r_montly] ")
   			// return pool.request().query("SELECT * FROM (SELECT SUBSTRING(r_montly, 1, 4)+'/'+SUBSTRING(r_montly, 5, 2) as r_montly,a_growth_rate,revenue FROM Revenue WHERE code='"+code+"' ) AS Revenue INNER JOIN (SELECT SUBSTRING(StockPrice.price_date, 1, 4)+'/'+SUBSTRING(StockPrice.price_date, 6, 2) AS [year],StockPrice.clos FROM (SELECT * FROM StockPrice WHERE code='"+code+"') AS StockPrice INNER JOIN ( SELECT code,maxdate = CASE DATEPART(dw,dateadd(ms,-3,DATEADD(mm,  DATEDIFF(m,0,price_date)+1,  0))) WHEN '1' THEN CONVERT(varchar(100),dateadd(dd,-2,dateadd(ms,-3,DATEADD(mm,  DATEDIFF(m,0,price_date)+1,  0))), 23) WHEN '7' THEN CONVERT(varchar(100),dateadd(dd,-1,dateadd(ms,-3,DATEADD(mm,  DATEDIFF(m,0,price_date)+1,  0))), 23) ELSE CONVERT(varchar(100),dateadd(ms,-3,DATEADD(mm,  DATEDIFF(m,0,price_date)+1,  0)), 23) END FROM StockPrice WHERE code='"+code+"' GROUP BY dateadd(ms,-3,DATEADD(mm,  DATEDIFF(m,0,price_date)+1,  0)),code) AS maxdata ON StockPrice.price_date = maxdata.maxdate) AS clos ON Revenue.r_montly = clos.[year] ORDER BY clos.[year]")
   		}).then(result => {
     		let rows = result.recordset
